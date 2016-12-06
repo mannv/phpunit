1c4b1858eaa692f188c074eb815e4a299e3975d4
vagrant <vagrant@homestead>
2016-12-06 07:54:08 +0000

    commit code

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -20,7 +20,7 @@ class ExampleController extends Controller
     }
 
     public function version() {
-        return response()->json(['version' => 'Day la phien ban moi 1.0']);
+        return response()->json(['version' => 'Day la phien ban moi 1.0 ----- 2.0']);
     }
 
     public function login(Request $request) {
