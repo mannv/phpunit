22038fd96e1e6c9ad59b0840f21c714b0af61708
man.nv <man.nv@kayac.vn>
2016-12-07 11:38:59 +0700

    fdhdfh

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -20,7 +20,7 @@ class ExampleController extends Controller
     }
 
     public function version() {
-        return '<h1>Test tu dong tao file .ENV haha</h1>';
+        return '<h1>Test tu dong tao file .ENV <span style=""color: red;>'.env('APP_ENV').'</span></h1>';
     }
 
     public function login(Request $request) {
