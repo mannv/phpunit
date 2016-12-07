624c55f45f172932dabb314bd97576a5fa83a61a
man.nv <man.nv@kayac.vn>
2016-12-07 17:37:13 +0700

    sdgsdgsdgsdg

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -28,7 +28,7 @@ class ExampleTest extends TestCase
 
 
     public function testLoginAction() {
-        $this->post('/login', ['email' => 'anhmantk'])->seeJsonStructure([
+        $this->post('/login', ['email' => 'anhmantk@gmail.com'])->seeJsonStructure([
             'errors' => ['email', 'password']
         ]);
     }
